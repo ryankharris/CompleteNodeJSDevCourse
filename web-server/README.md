d0766ee9d85ceeeb6854109d5fa8c9fe3b2377c2
Ryan K Harris <contact@ryankharris.com>
2022-02-17 17:22:17 -0800

    wip

---
+++ web-server/README.md
@@ -0,0 +1,3 @@
+# Web Server
+This part of the course begins to introduce express as a web-server. It introduces serving static assets. It suggests using handlebars as a templating engine. I think I will use Pug instead, in order to introduce a bit of a challenge.
+
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,14 +5,28 @@ const path = require('path');
 const publicPath = path.join(__dirname, '..', 'public');
 const app = express();
 const PORT = 3000;
+
+app.set('view engine', 'pug');
 app.use(express.static(publicPath));
 
-app.get('/help', (req, res) => {
-  res.send({
-    name: 'Ryan',
-    age: 41
-  });
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Welcome', message: "Welcome to Ryan's place (thanks to Pug)" });
 });
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About Me', message: "About me and stuff (thanks to Pug)", name: 'Ryan Harris' });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', { message: "Help for you (thanks to Pug)", name: 'Ryan Harris', age: 99 });
+});
+
+// app.get('/help', (req, res) => {
+//   res.send({
+//     name: 'Ryan',
+//     age: 99
+//   });
+// });
 
 app.get('/weather', (req, res) => {
   res.send({
