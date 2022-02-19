97de26c478ecd364de32188839b5d7a9272803a3
Ryan K Harris <contact@ryankharris.com>
2022-02-18 17:15:43 -0800

    added partial (nested) templates, via Pugs include feature

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,23 +2,32 @@
 const express = require('express');
 const path = require('path');
 
-const publicPath = path.join(__dirname, '..', 'public');
 const app = express();
 const PORT = 3000;
+const myName = 'Ryan Harris';
+const age = 99;
 
+// express path config
+const publicPath = path.join(__dirname, '..', 'public');
+const viewsPath = path.join(__dirname, '..', 'templates', 'views')
+
+// views with Pug config
 app.set('view engine', 'pug');
+app.set('views', viewsPath); // leaving it default is fine, so this isn't necessary
+
+// static assets config
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Welcome', message: "Welcome to Ryan's place (thanks to Pug)" });
+  res.render('index', { title: 'Welcome', headerMsg: "Welcome Page (thanks to Pug)", name: myName });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { title: 'About Me', message: "About me and stuff (thanks to Pug)", name: 'Ryan Harris' });
+  res.render('about', { title: 'About', headerMsg: "About Page (thanks to Pug)", name: myName });
 });
 
 app.get('/help', (req, res) => {
-  res.render('help', { message: "Help for you (thanks to Pug)", name: 'Ryan Harris', age: 99 });
+  res.render('help', { headerMsg: "Help Page (thanks to Pug)", name: myName, age });
 });
 
 // app.get('/help', (req, res) => {
