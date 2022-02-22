e7094ea1af2a2ab10a0f01ee560c911576290337
Ryan K Harris <contact@ryankharris.com>
2022-02-21 16:43:41 -0800

    wip

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -26,22 +26,31 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About', headerMsg: "About Page (thanks to Pug)", name: myName });
 });
 
+app.get('/about/*', (req, res) => {
+  res.status(404).render('notFound', {parentRoute: 'About', headerMsg: '404: About page not found', name: myName});
+});
+
 app.get('/help', (req, res) => {
   res.render('help', { headerMsg: "Help Page (thanks to Pug)", name: myName, age });
 });
 
-// app.get('/help', (req, res) => {
-//   res.send({
-//     name: 'Ryan',
-//     age: 99
-//   });
-// });
+app.get('/help/*', (req, res) => {
+  res.status(404).render('notFound', {parentRoute: 'Help', headerMsg: '404: Help page not found', name: myName});
+});
 
 app.get('/weather', (req, res) => {
   res.send({
     forecast: 'It is raining',
     location: 'Near Portland Oregon',
   });
+});
+
+app.get('/weather/*', (req, res) => {
+  res.status(404).render('notFound', {parentRoute: 'Weather', headerMsg: '404: Weather page not found', name: myName});
+});
+
+app.get('*', (req, res) => {
+  res.status(404).render('notFound', {parentRoute: '', headerMsg: '404: Page not found', name: myName});
 });
 
 app.listen(PORT, () => {
