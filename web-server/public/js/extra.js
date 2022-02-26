216de333b01fe0427d81c9ae3ddebb5d13a8968f
Ryan K Harris <contact@ryankharris.com>
2022-02-25 17:00:48 -0800

    finish weather app

--- web-server/public/js/extra.js
+++ web-server/public/js/extra.js
@@ -1,17 +1,26 @@
 
+
 const weatherForm = document.querySelector('form');
 weatherForm.addEventListener('submit', async (event) => {
+  const msg1 = document.querySelector('#msg-1');
+  const msg2 = document.querySelector('#msg-2');
   event.preventDefault();
+  msg1.textContent = 'Loading...';
+  msg2.textContent = '';
+  
   const searchInput = document.querySelector('input');
   const value = searchInput.value;
   const weatherPath = `http://localhost:3000/weather?address=${value}`;
   const response = await fetch(weatherPath);
   const data = await response.json();
   if (data.error) {
-    console.log('error:', data.error);
+    msg1.textContent = data.error;
     return;
   }
-  console.log(data);
+  
+  msg1.textContent = data.location;
+  const {chanceOfRain, currentTemp, description, feelsLikeTemp} = data.forecast || {};
+  msg2.textContent = `${currentTemp} (feels like ${feelsLikeTemp}) degrees and ${description} with ${chanceOfRain}% chance of rain.`;
 });
 
 // (async () => {
